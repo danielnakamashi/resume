c45a1320988a6814328c93d56bcabc71f00a165b
Daniel Nakamashi <daniel.nakamashi@auto1.com>
2020-05-16 15:06:25 +0200

    Updates

---
+++ precache-manifest.fd5e5adc1501815c9e171e790231c5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c96ab154c685c5323afb9bea30ff9bb",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "cb991c275efe1a7667c3",
+    "url": "/resume/static/css/main.88d9b9f6.chunk.css"
+  },
+  {
+    "revision": "1a147760367cb73e6f7e",
+    "url": "/resume/static/js/2.b3ff5caf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.b3ff5caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb991c275efe1a7667c3",
+    "url": "/resume/static/js/main.5400528e.chunk.js"
+  },
+  {
+    "revision": "bcaa68c0b5f7adc089f7",
+    "url": "/resume/static/js/runtime-main.05fbbc33.js"
+  }
+]);
